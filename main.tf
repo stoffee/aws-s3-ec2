3acf7a59f522b22fe066395a0b31aaa32355e711
stoffee <chris@roadkill.org>
2022-04-14 13:11:39 -0700

    public bucket

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "ec2_stoffee" {
   source  = "app.terraform.io/cdunlap/ec2-stoffee/aws"
   version = "1.0.5"
 
-  s3_bucket_acl = "public"
+  s3_bucket_acl = "public-read-write"
   s3_bucket_name = "stoaf"
   ssh_key_name = "cdunlap"
 }
