c72eff048cea7f6f367f81ccff6ac7338b5246fe
stoffee <chris@roadkill.org>
2022-04-14 14:04:34 -0700

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,10 @@
+//--------------------------------------------------------------------
+// Modules
+module "ec2_stoffee" {
+  source  = "app.terraform.io/cdunlap/ec2-stoffee/aws"
+  version = "1.0.6"
+
+  s3_bucket_acl = "private"
+  s3_bucket_name = "staof"
+  ssh_key_name = "cdunlap"
+}
