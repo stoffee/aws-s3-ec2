bf2b66ac025bea2494b09568a27f511477cc93c9
stoffee <chris@roadkill.org>
2022-04-14 13:10:17 -0700

    i want infra

---
+++ main.tf
@@ -0,0 +1,10 @@
+//--------------------------------------------------------------------
+// Modules
+module "ec2_stoffee" {
+  source  = "app.terraform.io/cdunlap/ec2-stoffee/aws"
+  version = "1.0.5"
+
+  s3_bucket_acl = "public"
+  s3_bucket_name = "stoaf"
+  ssh_key_name = "cdunlap"
+}
