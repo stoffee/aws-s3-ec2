4843f47a1e695aa705abd341aff1b79197ed64c2
stoffee <chris@roadkill.org>
2023-07-19 11:56:31 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "ec2_stoffee" {
   source  = "app.terraform.io/cdunlap/ec2-stoffee/aws"
   version = "1.0.6"
 
-  s3_bucket_acl = "private"
+  #s3_bucket_acl = "private"
   s3_bucket_name = "stoffee"
   ssh_key_name = "cdunlap-sandbox-aws"
 }
